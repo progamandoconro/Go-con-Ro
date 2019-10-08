d2a1f8ea8b808b0266332ac1e92091b8d282953c
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-08 02:29:36 +0100

    Update せーべー.go

--- せーべー.go
+++ せーべー.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-	"log"
-	"net/http"
+ "net/http"
+  "fmt"
+  "time"
+
 )
 
 func main() {
-	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/usr/share/doc"))))
+   http.HandleFunc("/", HelloServer)
+   http.ListenAndServe(":8585", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+ y := time.Now()
+ z := y.Hour()
+
+ if  z < 19 && z > 6 {
+
+       fmt.Fprintf(w, "こにちわ", r.URL.Path[1:])
+} else {
+
+        fmt.Fprintf(w, "こんばんわ", r.URL.Path[1:])
+}
+
 }
