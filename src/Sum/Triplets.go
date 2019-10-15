4fe48dc0c5cd19e56137c587d6d138745b25ee50
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-15 21:16:22 +0100

    Update Triplets.go

--- src/Sum/Triplets.go
+++ src/Sum/Triplets.go
@@ -1,53 +1,50 @@
 package main
-
 import "fmt"
 
 var a1, a2, a3, b1, b2, b3 int
+var ra1, ra2, ra3, rb1, rb2, rb3 int
 
 func main() {
-	var a1, a2, a3, b1, b2, b3 int
-        fmt.Scanln(&a1)
-        fmt.Scanln(&a2)
-        fmt.Scanln(&a3)
-        fmt.Scanln(&b1)
-        fmt.Scanln(&b2)
-        fmt.Scanln(&b3)
-        if a1 == b1 {
-                a1, b1 = 0, 0
-        }
-	if a2 == b2 {
-                a2, b2 = 0, 0
-        }
-	if a3 == b3 {
 
-                a3, b3 = 0, 0
+	fmt.Scanln(&a1, &a2, &a3 ,&b1, &b2, &b3)
+
+        // Round 1
+        if a1 == b1 {
+                ra1 , rb1 = 0 , 0
         }
 
 	if a1 > b1 {
-                a1, b1 = 1, 0
-        }
-
-	if a2 > b2 {
-                a2, b2 = 1, 0
-        }
-
-	if a3 > b3 {
-                a3, b3 = 1, 0
+                ra1 , rb1 = 1 , 0
         }
 
 	if a1 < b1 {
-                a1, b1 = 0, 1
+                ra1 , rb1 = 0,  1
+        }
+	//Round 2
+        if a2 == b2 {
+                ra2, rb2 = 0 , 0
         }
 
 	if a2 > b2 {
-                a2, b2 = 0, 1
+                ra2 ,rb2 = 1 ,  0
+        }
+
+
+	if a2 < b2 {
+                ra2 , rb2 =  0 , 1
+        }
+        //Round 3
+        if a3 == b3 {
+
+                ra3 , rb3 = 0 , 0
         }
 
 	if a3 > b3 {
-                a3, b3 = 0, 1
+                ra3 , rb3 = 1 , 0
         }
 
-	fmt.Println((a1 + a2 + a3), (b1 + b2 + b3))
+        if a3 < b3 {
+                ra3 , rb3 = 0,  1
+        }
 
-
-// not working yet
+        fmt.Println(ra1 + ra2 + ra3 , rb1 + rb2 + rb3)
