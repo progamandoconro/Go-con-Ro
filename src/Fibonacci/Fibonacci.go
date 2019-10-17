3b983d45a419d9e1319598cadddb780aa83b6387
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-17 14:28:12 +0100

    Update Fibonacci.go

--- src/Fibonacci/Fibonacci.go
+++ src/Fibonacci/Fibonacci.go
@@ -8,23 +8,28 @@ func Fibonacci(n uint) uint64 {
         if n <= 1 {
                 return uint64(n)
         }
-        
+
         var n2, n1 uint64 = 0, 1
-        
+
         for i := uint(2); i < n; i++ {
                 n2, n1 = n1, n1+n2
         }
-        
+
         return n2 + n1
 }
 
 func main (){
+        var nth uint
+        fmt.Scan(&nth)
+        time.Sleep(time.Second * 10 )
 
-        for i:= uint(1); i<1000;i++{
-                
+        for i:= uint(1); i<nth;i++{
+
         fmt.Println(Fibonacci(i))
         time.Sleep(500 * time.Millisecond)
-}
+        }
 
 }
+
+
 
